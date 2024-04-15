752a1a839714c960aff5b0b92bc8db5f4d676782
broklyn <100339728+bbroklyn@users.noreply.github.com>
2024-04-15 13:12:41 +0500

    assets

---
+++ src/cursor/cursor.js
@@ -0,0 +1,13 @@
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
+
+window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.style.left = `${posX - 15}px`;
+    cursorOutline.style.top = `${posY - 15}px`;
+});
